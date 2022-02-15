f72c24b8e1a5697d1a1f11527a420d5ba712d4f8
River Kelly <65210753+RK311y@users.noreply.github.com>
2022-02-15 06:38:45 -0700

    Update code

--- main.py
+++ main.py
@@ -60,26 +60,25 @@ data[6] = np.array(['blue',   'no',   'west'])
 oneHotData = np.ndarray((7, 9))
 
 for row in range(data.shape[0]):
-    row_array       = data[row,:]
-    
-    hot_data_row    = np.array([0] * 9)
-
-    color           = row_array[0]
-    yes_no          = row_array[1]
-    direction       = row_array[2]
-    
+    row_array = data[row,:]
+    color       = row_array[0]
+    yes_no      = row_array[1]
+    direction   = row_array[2]
+    # empty row for encoded data
+    hot_data_row = np.array([0] * 9)
+    # encode color
     if color == b'red': hot_data_row[0] = 1
     elif color == b'blue': hot_data_row[1] = 1
     elif color == b'yellow': hot_data_row[2] = 1
-
+    # encode yes/no
     if yes_no == b'yes': hot_data_row[3] = 1
     else: hot_data_row[4] = 1
-
+    # encode direction (north, south, east, and west)
     if   direction == b'north': hot_data_row[5] = 1
     elif direction == b'south': hot_data_row[6] = 1
     elif direction == b'east': hot_data_row[7] = 1
     elif direction == b'west': hot_data_row[8] = 1
-    
+    # set row values on encoded matrix
     oneHotData[row] = hot_data_row
 
 print("Question 2: \n", oneHotData, '\n')
@@ -94,94 +93,53 @@ def euclideanDistance(m, row1Num, row2Num):
     for i in range(m.shape[1]):
         answer += (x1[i] - x2[i]) ** 2
     return math.sqrt(answer)
-    pass
 
 print("Question 3.) - Euclidean Distance (x2 and x7): ", euclideanDistance(oneHotData, 1, 6), '\n')
 
 # Question 4 - cosine similarity (cosine of the angle)
 # --------------------------------------------------------------------
 
-# data = np.ndarray((7, 9))
-
-# #                  |       |     |           |
-# data[0] = np.array([1, 0, 0, 1, 0, 1, 0, 0, 0])
-# #                  |       |     |           |
-# data[1] = np.array([0, 1, 0, 0, 1, 0, 1, 0, 0])
-# #                  |       |     |           |
-# data[2] = np.array([0, 0, 1, 0, 1, 0, 0, 1, 0])
-# #                  |       |     |           |
-# data[3] = np.array([0, 0, 1, 0, 1, 0, 0, 0, 1])
-# #                  |       |     |           |
-# data[4] = np.array([1, 0, 0, 1, 0, 1, 0, 0, 0])
-# #                  |       |     |           |
-# data[5] = np.array([0, 0, 1, 1, 0, 1, 0, 0, 0])
-# #                  |       |     |           |
-# data[6] = np.array([0, 1, 0, 0, 1, 0, 0, 0, 1])
-
-
-x2 = oneHotData[1,:]
-x7 = oneHotData[6,:]
-
-print('X2: ',x2)
-print('X7: ', x7)
-
-
-def oneHotDistance():
-    answer = 0
-    for i in range(len(x2)):
-        a = x2[i]
-        b = x7[i]
-        answer += (a-b) ** 2
-    return math.sqrt(answer)
-
-print("One Hot Distance: ", oneHotDistance())
-
-
-def cosineX2andX7():
+def cosineOfRows(m, row1Num, row2Num):
+    x1 = m[row1Num,:]
+    x2 = m[row2Num,:]
     top = 0
-    for i in range(len(x2)):
-        top += x2[i] * x7[i]
     bottom_left = 0
     bottom_right = 0
-    for i in range(len(x2)):
-        bottom_left += x2[i] ** 2
-        bottom_right += x7[i] ** 2
-    bottom_left = math.sqrt(bottom_left)
-    bottom_right = math.sqrt(bottom_right)
-    answer = top / (bottom_left * bottom_right)
-    return answer
-print("Cosine x2 and x7: ", cosineX2andX7())
+    for i in range(m.shape[1]):
+        top += x1[i] * x2[i]
+        bottom_left += x1[i] ** 2
+        bottom_right += x2[i] ** 2
+    return top / (math.sqrt(bottom_left) * math.sqrt(bottom_right))
 
+print("Cosine x2 and x7: ", cosineOfRows(oneHotData, 1, 6))
 
-def xorOfx2andx7():
+# Question 5 - Hamming Distance
+# --------------------------------------------------------------------
+
+def hammingDistance(m, row1Num, row2Num):
+    x1 = m[row1Num,:]
+    x2 = m[row2Num,:]
     sum = 0
-    for i in range(len(x2)):
-        if (x2[i] == 1 and x7[i] == 0) or (x2[i] == 0 and x7[i] == 1):
+    for i in range(m.shape[1]):
+        if (x1[i] == 1 and x2[i] == 0) or (x1[i] == 0 and x2[i] == 1):
             sum += 1
     return sum
 
-print("XOR of x2 and x7", xorOfx2andx7())
+print("Hamming Distance (XOR) of x2 and x7: ", hammingDistance(oneHotData, 1, 6))
 
+# Question 6 - Jaccard similarity
+# --------------------------------------------------------------------
 
-def JACCARDOfx2andx7():
+def jaccardSimilarity(m, row1Num, row2Num):
+    x1 = m[row1Num,:]
+    x2 = m[row2Num,:]
     top = 0
     bottom = 0
-    for i in range(len(x2)):
+    for i in range(m.shape[1]):
         # top
-        if (x2[i] == x7[i]): top += 1
+        if (x1[i] == x2[i]): top += 1
         # bottom
-        if (x2[i] != x7[i]): bottom += 1
+        if (x1[i] != x2[i]): bottom += 1
     return (top / bottom)
+print("Jaccard similarity of x2 and x7: ", jaccardSimilarity(oneHotData, 1, 6))
 
-print("JACCARDOfx2andx7():", JACCARDOfx2andx7())
-
-
-# def multiMean():
-#     multi_mean = []
-#     for col in range(data.shape[1]): # rows
-#         multi_mean[col] = data[:,col].mean()
-#         # for col in range(data.shape[1]): # cols
-#             # multi_mean[row] 
-#     return multi_mean
-
-# print(multiMean())
